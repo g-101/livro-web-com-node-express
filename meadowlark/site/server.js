a7a09d140d822211b62795d0f1c3eeb7cbcd00cc
genesis-g101 <gyreplicant@gmail.com>
2023-04-09 10:17:44 -0300

    views e layouts

--- meadowlark/site/server.js
+++ meadowlark/site/server.js
@@ -1,34 +1,32 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars'); // view engine
 
 const app = express();
 const port = process.env.PORT || 3333;
+
+// O que são Views?
+// qualquer coisa que pode ser renderizada no lado do cliente
+// pdf, html, png
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // app.METHOD recebe um path e um callback
 // o path define a rota(não é case sensitive)
 // quando a rota for encontrada, o callback é chamado
 // o callback recebe dois parametros: objetos de requisição e resposta.
-app.get('/', (request, response) => {
-  response.type('text/plain');
-  response.send('Meadowlark Travel');
-});
-app.get('/about', (request, response) => {
-  response.type('text/plain');
-  response.send('About Meadowlark Travel');
-});
+app.get('/', (request, response) => response.render('home'));
+app.get('/about', (request, response) => response.render('about'));
 
 // app.use é o metodo que o express adiciona middleware
 // a ordem que as rotas e middleware estão IMPORTA
 app.use((request, response) => {
-  response.type('text/plain');
   response.status(404);
-  response.send('404 - not found');
+  response.render('404');
 });
-
 app.use((error, request, response, next) => {
-  console.error(error);
-  response.type('text/plain');
+  console.error(error.message);
   response.status(505);
-  response.send('505 - internal error');
+  response.render('505');
 });
 
 app.listen(port, () => console.log(`express started on http://localhost:${port} press Ctrl-C to terminate`));
